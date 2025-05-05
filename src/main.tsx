e0f711d141f6f04c5bda259d3654b4b90488403a
JordyBacherot <jordy.bacherot@outlook.fr>
2025-05-05 14:09:53 +0200

    Revert "Add vercel speed insights for performance stat"
    This reverts commit 489dbba00f7acf66b39c7f281975557a8664d244.

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import App from "./App";
 import "./index.css";
 import {BrowserRouter} from "react-router";
 import {AuthProvider} from "@/contexts/AuthProvider.tsx";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <AuthProvider>
                 <App />
-                <SpeedInsights/>
             </AuthProvider>
         </BrowserRouter>
     </StrictMode>
